af132b0202f06b00691d300d40daf1a5b4e753d6
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-05-19 18:46:34 +0700

    feat: add create project feature

--- src/components/leftbar.js
+++ src/components/leftbar.js
@@ -1,17 +1,6 @@
 import todoCore from "../model/todo.core.js";
 import { createElement } from "../utils/dom.js";
-
-// user project data
-let projectItems = todoCore.project.getUserProjects();
-projectItems = projectItems.map((project) => {
-  const safeName = project.name.split(" ").join(" ");
-  project.target = `/project/${safeName}--${project.id}`;
-  return project;
-});
-const userProjects = {
-  title: "Projects",
-  items: projectItems,
-};
+import { renderProjectForm } from "./project-form.js";
 
 const filters = {
   title: "",
@@ -25,7 +14,7 @@ function renderNavSection({ title, items }) {
   const navSection = createElement("div", "leftbar-nav");
 
   if (title) {
-    navSection.append(createElement("div", "leftbar-nav-header", title));
+    navSection.append(createElement("div", "leftbar-nav-header-title", title));
   }
 
   items.forEach((item) => {
@@ -45,6 +34,60 @@ function renderNavSection({ title, items }) {
   return navSection;
 }
 
+function renderProjectNavSection() {
+  // user project data
+  let projectItems = todoCore.project.getUserProjects();
+  projectItems = projectItems.map((project) => {
+    const safeName = project.name.split(" ").join(" ");
+    project.target = `/project/${safeName}--${project.id}`;
+    return project;
+  });
+
+  const data = {
+    title: "Projects",
+    items: projectItems,
+  };
+
+  const NAV_ID = "nav-projects";
+  let navSection;
+  navSection = document.querySelector(`[data-id="${NAV_ID}"]`);
+  if (navSection) {
+    navSection.textContent = "";
+  } else {
+    navSection = createElement("div", "leftbar-nav");
+    navSection.dataset.id = NAV_ID;
+  }
+
+  const navSectionHeader = createElement("div", "leftar-nav-header");
+
+  navSection.append(
+    createElement("div", "leftbar-nav-header-title", data.title)
+  );
+
+  const onSubmit = (data) => {
+    todoCore.project.createProject(data);
+    renderProjectNavSection();
+  };
+  const projectForm = renderProjectForm({}, onSubmit);
+
+  const navItems = data.items.map((item) => {
+    const navItem = createElement(
+      "div",
+      `leftbar-nav-item ${item.disable ? " disable" : ""}`,
+      item.name
+    );
+    navItem.dataset.target = item.target;
+
+    navItem.addEventListener("click", () => {
+      window.location.hash = item.target;
+    });
+    return navItem;
+  });
+
+  navSection.append(navSectionHeader, projectForm, ...navItems);
+  return navSection;
+}
+
 export function renderLeftbar() {
   const leftbar = createElement("div", "leftbar");
 
@@ -57,7 +100,7 @@ export function renderLeftbar() {
   leftbar.append(
     leftbarHeader,
     renderNavSection(filters),
-    renderNavSection(userProjects)
+    renderProjectNavSection()
   );
 
   return leftbar;
