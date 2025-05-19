5cd91cf32b9ffab6be780312294354fce69ee682
fath-nasrudin <fath.nasrudin.i@gmail.com>
2025-05-19 15:36:49 +0700

    Refactor inbox page to simplify rendering by removing task-related components

--- src/page/inbox.page.js
+++ src/page/inbox.page.js
@@ -1,28 +1,7 @@
-import { renderTaskForm } from "../components/task-form.js";
-import {
-  renderTasksContainer,
-  renderTasksContainerWrapper,
-  renderTasksFilterTitle,
-  renderTasksSection,
-} from "../components/tasks.js";
 import todoCore from "../model/todo.core.js";
+import { renderProjectPage } from "./project.page.js";
 
 export function renderInboxPage() {
   const projectId = todoCore.project.getDefaultProjectId();
-  const project = todoCore.project.getProjectById(projectId);
-
-  const tasks = todoCore.task.getTasksByProjectId(project.id);
-  function onSubmit(data) {
-    todoCore.task.createTask(data);
-    renderProjectPage(projectId);
-  }
-
-  const taskForm = renderTaskForm({}, onSubmit);
-  const tasksFilterTitle = renderTasksFilterTitle(project.name);
-  const sections = [{ tasks }].map((section) =>
-    renderTasksSection(null, section.tasks)
-  );
-
-  const tasksContainer = renderTasksContainerWrapper();
-  tasksContainer.append(tasksFilterTitle, taskForm, ...sections);
+  renderProjectPage(projectId);
 }
